4a8df73b1bab48d8bb03f0ad1cd062b5ddfe34cb
ZzyzxIsAlreadyTaken <theoz_@hotmail.com>
2025-01-09 13:57:05 +0100

    Added netlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'server',
+  adapter: netlify(),
 });
